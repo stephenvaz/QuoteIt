1e40be552863692c93d6e4c4d44a24e2162c97a4
Atharv Salian <69430707+Atharv56@users.noreply.github.com>
2023-02-16 14:44:16 +0530

    update

--- asteroid/src/components/image3.js
+++ asteroid/src/components/image3.js
@@ -1,9 +1,9 @@
 import { Parallax } from 'react-parallax';
 import './image.css'
 import { Link } from 'react-router-dom';
-import Ast2 from '../images/asteroid2.jpg'
+import analytics from '../images/analytics.jpg'
 const Image3 = () => (
-    <Parallax className='image' bgImage= {Ast2} strength={800}>
+    <Parallax className='image' bgImage= {analytics} strength={800}>
         <div className= "content">
             <Link to='/predict'>
             <button className='img-btn'>Suggest me some good quotes</button>
